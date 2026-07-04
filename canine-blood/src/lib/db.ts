ff7673406487651eb6cc21c1e7d1e9a4bbc69b53
Vallad1122 <johnbattern81@gmail.com>
2026-07-04 16:15:03 +1000

    Update db.ts

--- canine-blood/src/lib/db.ts
+++ canine-blood/src/lib/db.ts
@@ -10,8 +10,8 @@ if (missingEnv.length > 0) {
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 3306),
-  user: "root",
-  password: "8143123@Tai",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
